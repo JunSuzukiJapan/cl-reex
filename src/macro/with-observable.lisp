4f765b4087c741e9df9fe12001b4a3ef68721eaf
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2020-12-23 15:02:21 +0900

    remove comment out lines

--- src/macro/with-observable.lisp
+++ src/macro/with-observable.lisp
@@ -31,21 +31,6 @@
 	   (var-name (gensym)) )
       (dolist (x body)
 	(case (car x)
-#|	  ;;
-	  ;; Where
-	  ;;
-	  ('where
-	   (setq var-name (gensym))
-	   ;; (push `(,var-name
-	   ;; 	   (make-operator-where
-	   ;; 	    ,temp-observable
-	   ;; 	    #'(lambda ,(cadr x) ,(caddr x) )))
-	   ;; 	 lst )
-	   (let* ((op (get-operator :where))
-		  (elem (funcall op x var-name temp-observable)) )
-	     (push elem lst) )
-	   (setq temp-observable var-name) )
-|#
 	  ;;
 	  ;; Subscribe
 	  ('subscribe
@@ -77,66 +62,3 @@
 	 ,var-name ))))
 
 
-#|
-(setq body '(
-  (where (x) (evenp x))
-  (where (x) (eq (mod x 3) 0))
-  (subscribe observer) ))
-(macroexpand-1 '(rx:with-observable observable
-  (rx:where (x) (evenp x))))
-|#
-
-
-#|
-(defvar ol (rx:observable-from '(1 2 3 4 5 6 7 8 9 10)))
-(defvar observer (rx:make-observer
-		#'(lambda (x) (print x))
-		#'(lambda (x) (format t "error: ~S" x))
-		#'(lambda () (print "completed")) ))
-(rx:subscribe ol observer)
-
-(defvar op)
-(setq op (rx:make-operator-where ol #'(lambda (x) (evenp x))))
-(rx:subscribe op observer)
-
-(defvar op2)
-(setq op2 (rx:make-operator-where op #'(lambda (x) (eq (mod x 3) 0))))
-
-(let* ((temp-observable ol)
-       (where-op1 (rx:make-operator-where temp-observable
- 				       #'(lambda (x) (evenp x))))
-       (where-op2 (rx:make-operator-where where-op1
-				       #'(lambda (x) (eq (mod x 3) 0))))
-       (temp (rx:subscribe where-op2 observer)) )
-  temp )
-|#
-
-#|
-(macroexpand-1 '(rx:with-observable ol
-  (rx:where (x) (evenp x))
-  (rx:where (x) (eq (mod x 3) 0))
-  (rx:subscribe observer)
-  (rx:dispose) ))
-
- (rx:with-observable ol
-  (rx:where (x) (evenp x))
-  (rx:where (x) (eq (mod x 3) 0))
-  (rx:subscribe observer)
-  (rx:dispose) )
-|#
-
-#|
-(with-observable observable
-  (where (x) (evenp x))
-  (where (x) (eq (mod x 3) 0))
-  (subscribe observer) )
-
-
-(let* ((temp-observable observable)
-       (where-op1 (rx:make-operator-where temp-observable
- 				       #'(lambda (x) (evenp x))))
-       (where-op2 (rx:make-operator-where where-op1
-				       #'(lambda (x) (eq (mod x 3) 0))))
-       (temp (rx:subscribe where-op2 observer)) )
-  temp )
-|#
