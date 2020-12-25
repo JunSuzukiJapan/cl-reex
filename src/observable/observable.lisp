e2ca17bdf0d0c25d894b0354527303b261eb26de
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2020-12-25 11:20:26 +0900

    fix: in loop statement, use not observable-array but (source observable-array)

--- src/observable/observable.lisp
+++ src/observable/observable.lisp
@@ -24,6 +24,9 @@
 
 (deftype observable-state () '(active error completed))
 
+;;
+;; observable from list
+;;
 (defclass observable-list ()
   ((src-list :initarg :src-list
 	     :initform nil
@@ -49,7 +52,40 @@
 (defmethod observable-from ((source list))
   (make-instance 'observable-list :src-list source))
 
-;(defmethod observable-from ((source string)))
+;;
+;; observable from array
+;;
 ;(defmethod observable-from ((source array)))
+(defclass observable-array ()
+  ((source :initarg :source
+	   :initform #()
+	   :accessor source)) )
+
+(defclass disposable-observable-array ()
+  ((observable :initarg :observable
+	       :accessor observable)
+   (observer :initarg :observer
+	     :accessor observer) ))
+
+(defmethod subscribe ((ary observable-array) observer)
+  (loop for item across (source ary)
+     do (funcall (on-next observer) item) )
+  (funcall (on-completed observer))
+  (make-instance 'disposable-observable-array
+		 :observable ary
+		 :observer observer ))
+
+(defmethod dispose ((dis-ary disposable-observable-array)))
+
+(defmethod observable-from ((source array))
+  (make-instance 'observable-array :source source) )
+
+
+
+;(defmethod observable-from ((source string)))
 ;(defmethod observable-from ((source stream)))
 ;(defmethod observable-from ((source fundamental-input-stream)))
+
+
+
+;(defun observable-range (from count))
